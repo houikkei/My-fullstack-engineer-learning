03afe798be7a6df59594cc1e782672156dffb628
ほういっけい <69685486+hoikkei@users.noreply.github.com>
2024-05-30 00:16:12 +0800

    Add files via upload

---
+++ My-frontend-learning-basic/单词（新）/js/css_day06.js
@@ -0,0 +1,27 @@
+var css_day06 = [
+  ['position', '[pəˈzɪʃn]', 7, '元素定位', '定位属性'],
+  ['relative', '[ˈrelətɪv]', 8, '相对定位', '元素不脱标'],
+  ['absolute', '[ˈæbsəluːt]', 8, '绝对定位', '元素脱标'],
+  ['fixed', '[fɪkst]', 8, '固定定位', '元素脱标'],
+  ['z-index', ' [ziː ˈɪndeks] ', 7, '层级', '定位元素的叠放顺序'],
+  ['visibility', '[ˌvɪzəˈbɪləti]', 7, '元素显示方式', '默认值visible'],
+  ['visible', ' [ˈvɪzəbl]', 8, '元素显示方式', '默认显示'],
+  ['overflow', "[,ovɚ'flo]", 7, '溢出;', '-'],
+  ['hidden', "['hɪdn]", 8, '隐藏;', '-'],
+  ['cursor', '[ˈkɜːsə(r)]', 7, '鼠标样式', '-'],
+  ['pointer', '[ˈpɔɪntə(r)] ', 8, '鼠标变成小手形状', '鼠标放在元素上显示小手'],
+  ['vertical-align', '[ˈvɜːtɪkl əˈlaɪn', 7, '-', '行内块、行内元素对齐方式'],
+  ['baseline', '[ˈbeɪslaɪn]', 8, '基线', '行内块默认基线对齐'],
+  ['middle', '[ˈmɪdl]', 8, '中线', '行内块元素与文字中线对齐'],
+  ['border-radius', '[ˈbɔːdə(r)][ˈreɪdiəs]', 7, '边框圆角'],
+  ['border-collapse:collapse;', '[ˈbɔːdə(r)][ˈreɪdiəs]', 7, '合并表格相邻边框'],
+]
+
+var css_day07 = [
+  ['outline:none', '[ˈaʊtlaɪn]', 7, '清除表单元素轮廓线'],
+  ['background-size', '[ˈbækɡraʊnd][saɪz]', 7, '设置背景图片大小'],
+  ['cover', '[ˈkʌvər]', 8, '覆盖'],
+  ['contain', '[kənˈteɪn]', 8, '包含'],
+  ['box-shadow', '[bɒks][ˈʃædəʊ]', 7, '盒子阴影'],
+  ['transition', '[trænˈzɪʃn]', 7, '过渡', '两个状态, css属性值的变化'],
+]
